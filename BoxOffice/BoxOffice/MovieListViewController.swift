fd668962344bcea9406365d56a990dc96c43eba2
doyeon326 <doyeon326@gmail.com>
2020-09-22 20:57:15 +0900

    Add files via upload

---
+++ BoxOffice/BoxOffice/MovieListViewController.swift
@@ -0,0 +1,74 @@
+//
+//  MovieListViewController.swift
+//  BoxOffice
+//
+//  Created by Tony Jung on 2020/09/22.
+//  Copyright © 2020 com.doyeon. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class MovieListViewController: UIViewController {
+
+    var movies: [Movie] = [] //mvvc패턴으로 구현해야함
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        ParseAPI.loadMovies { movies in
+             print("몇개? \(movies.count), 첫번째 제목 \(movies.first?.title)")
+            DispatchQueue.main.async {
+                            self.movies = movies
+                            self.collectionView.reloadData()
+                        }
+        }
+    }
+}
+
+
+extension MovieListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as? CollectionCell else { return UICollectionViewCell() }
+        let movie = movies[indexPath.item]
+        let url = URL(string: movie.thumb)!
+        cell.image.kf.setImage(with: url)
+        cell.date.text = movie.date
+        cell.rank.text = "\(movie.reservationGrade)위(\(movie.userRating))/ \(movie.reservationRate)%"
+        cell.title.text = movie.title
+        return cell
+    }
+}
+
+extension MovieListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //눌렀을때
+    }
+}
+
+extension MovieListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+               let itemSpacing: CGFloat = 10
+               let textAreaHeight: CGFloat = 65
+               let width: CGFloat = (collectionView.bounds.width - itemSpacing)/2
+               let height: CGFloat = width * 10/7 + textAreaHeight
+               return CGSize(width: width, height: height)
+    }
+}
+
+class CollectionCell: UICollectionViewCell {
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var rank: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var image: UIImageView!
+}
---
+++ BoxOffice/BoxOffice/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  BoxOffice
+//
+//  Created by Tony Jung on 2020/09/21.
+//  Copyright © 2020 com.doyeon. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [Movie] = []
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        ParseAPI.loadMovies { movies in
+                print("몇개? \(movies.count), 첫번째 제목 \(movies.first?.title)")
+                DispatchQueue.main.async {
+                    self.movies = movies
+                    self.tableView.reloadData()
+                }
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as? TableCell else {
+            return UITableViewCell()
+        }
+        let movie = movies[indexPath.item]
+        let url = URL(string: movie.thumb)!
+        
+        cell.movieImage.kf.setImage(with: url)
+        cell.title.text = movies[indexPath.row].title
+        cell.date.text = "개봉일: \(movies[indexPath.row].date)"
+        cell.detail.text = "평점: \(movies[indexPath.row].userRating) 예매순위: \(movies[indexPath.row].reservationGrade) 예매율: \(movies[indexPath.row].reservationRate)"
+        return cell
+    }
+}
+
+class TableCell: UITableViewCell {
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var detail: UILabel!
+    @IBOutlet weak var title: UILabel!
+    //circle & label color need to be change
+}
+
+struct Response: Codable {
+    let movies: [Movie]
+}
+
+struct Movie: Codable {
+    let date: String
+    let title: String
+    let userRating: Double
+    let reservationRate: Double
+    let thumb: String
+    let reservationGrade: Int
+    
+    enum CodingKeys: String, CodingKey{
+        case date
+        case title
+        case userRating = "user_rating"
+        case reservationRate = "reservation_rate"
+        case thumb
+        case reservationGrade = "reservation_grade"
+    }
+    
+    
+}
+
+
